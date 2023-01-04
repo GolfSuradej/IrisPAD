590bb99255b489799d4668abb5c027e8844ad794
Suradej Duangpummet <suradej.duangpummet@nectec.or.th>
2023-01-04 14:08:27 +0700

    Update ExtractBISF.py

--- ExtractBISF.py
+++ ExtractBISF.py
@@ -12,13 +12,14 @@ if __name__ == "__main__":
     
     path = "/home/suradej/OpenSourceIrisPAD-master/images"
     i = 0
-    myBISF = np.zeros([30,32,1])
-    
+    buff = np.zeros([30,32,1])
+
     for file in os.listdir(path):
         imgfile = os.path.join(path,file)
         src = cv2.imread(imgfile,flags=0)
         hist = np.asarray(generateHistogram(src,3,5))
-        myBISF[i] = np.reshape(hist,[32,1])
+        buff[i] = np.reshape(hist,[32,1])
         i = i+1
 
+    myBISF = buff[0:i]
     np.save("myBISF.npy",myBISF)
